7291beade55062ee3edfdca64106e01185e735da
NicolleDanielaSarmientoSerranoADSO <daniisarmientose26@gmail.com>
2022-09-21 12:23:18 -0500

    Ejercicio 6

---
+++ main6.js
@@ -0,0 +1,7 @@
+addEventListener("DOMContentLoaded",()=>{
+   let tem=Number(prompt(`Ingrese la temperatura:`))
+   let pres=Number(prompt(`Ingrese la presión:`))
+   if(tem>100){
+    document.write
+   }
+})
