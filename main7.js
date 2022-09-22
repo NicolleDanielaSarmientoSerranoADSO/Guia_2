c95e586d0752d6348f1fdf7b628e57373a6e977d
NicolleDanielaSarmientoSerranoADSO <daniisarmientose26@gmail.com>
2022-09-22 10:02:44 -0500

    Guia2

--- main7.js
+++
@@ -1,6 +0,0 @@
-addEventListener("DOMContentLoaded",()=>{
-   let radio=0, v=4/3
-    radio=Number(prompt("Ingresa el radio de la esfera",3));
-    let vesfera=(v*3.14*radio**2);
-    console.log(`la respuesta es:  ${vesfera}`);
-})
